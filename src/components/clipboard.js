0168615e2415624d7130da32cd791ed50a28a367
阿狼 <1280597658@qq.com>
2017-09-09 19:15:09 +0800

    add clipboard

---
+++ src/components/clipboard.js
@@ -0,0 +1,21 @@
+import React, {Component, PropTypes} from 'react'
+import ReactDOM from "react-dom";
+import Clipboard from "imoney-react-ui/src/clipboard";
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className="">
+                <Clipboard text="test" handleText="copy" success={()=>{alert("success!")}}/>
+            </div>
+        )
+    }
+}
+export default App;
